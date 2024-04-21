cdc6f195549b06223731caf97701b70f4c2a4c1a
Jacob <jacob13smith@gmail.com>
2024-04-21 15:42:49 -0700

    forked 2 threads for p2p listeners

--- daemon/src/lib/network.rs
+++ daemon/src/lib/network.rs
@@ -28,35 +28,45 @@ pub async fn initialize_p2p_thread(mut receiver_from_blockchain: Receiver<String
     if let Some(key_pair) = keys {
         let rsa_pkey_bytes = key_pair.private_key.clone();
         let private_key = PKey::private_key_from_pkcs8(&rsa_pkey_bytes).unwrap();
+        let private_key_clone = private_key.clone();
+        let sender_clone = sender_to_blockchain.clone();
 
-        let _ = tokio::spawn(handle_request_from_blockchain(receiver_from_blockchain, sender_to_blockchain.clone(), private_key.clone())).await.unwrap();
+        let blockchain_listener = tokio::spawn(async move {
+            handle_request_from_blockchain(receiver_from_blockchain, sender_clone, private_key_clone).await;
+        });
 
-        // Build SSL Acceptor with saved RSA key and no cert verification
-        let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-        acceptor.set_private_key(&private_key).unwrap();
-        acceptor.set_verify(SslVerifyMode::NONE);
-        let acceptor = acceptor.build();
-
-        let listener = TcpListener::bind("0.0.0.0:8047").unwrap();
-
-        println!("Waiting on peer-to-peer connections...");
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            let mut ssl_stream = acceptor.accept(stream).unwrap();
-
-            let mut buf = [0u8; 1024];
-            let bytes_read = ssl_stream.ssl_read(&mut buf).unwrap();
-
-            let data = &buf[..bytes_read];
-            println!("Received data: {:?}", data);
-        }
+        let network_listener = tokio::spawn(async move {
+            handle_request_from_network(sender_to_blockchain, private_key).await;
+        });
     }
 
 }
 
+async fn handle_request_from_network(mut sender_to_blockchain: Sender<String>, private_key: PKey<Private>){
+    println!("Waiting on peer-to-peer connections...");
+    // Build SSL Acceptor with saved RSA key and no cert verification
+    let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    acceptor.set_private_key(&private_key).unwrap();
+    acceptor.set_verify(SslVerifyMode::NONE);
+    let acceptor = acceptor.build();
+
+    let listener = TcpListener::bind("0.0.0.0:8047").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let mut ssl_stream = acceptor.accept(stream).unwrap();
+
+        let mut buf = [0u8; 1024];
+        let bytes_read = ssl_stream.ssl_read(&mut buf).unwrap();
+
+        let data = &buf[..bytes_read];
+        println!("Received data: {:?}", data);
+    }
+}
+
 async fn handle_request_from_blockchain(mut receiver_from_blockchain: Receiver<String>, sender_to_blockchain: Sender<String>, private_key: PKey<Private>) {
+    println!("Waiting on message from blockchain to network");
     loop {
-        println!("Waiting on message from blockchain to network");
         if let Some(msg) = receiver_from_blockchain.recv().await {
             println!("Recieved message from blockchain to network");
             let blockchain_request: P2PRequest = from_str(&msg).unwrap();
