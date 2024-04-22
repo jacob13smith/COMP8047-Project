263383a3821e249ab4ce1a92035329744d76ef11
Jacob <jacob13smith@gmail.com>
2024-04-21 21:31:06 -0700

    just testing

--- daemon/src/lib/network.rs
+++ daemon/src/lib/network.rs
@@ -1,11 +1,12 @@
 use std::{collections::hash_map::DefaultHasher, hash::{Hash, Hasher}, io::{Cursor, Read, Write}, net::{TcpListener, TcpStream}, sync::Arc, time::Duration};
 use openssl::pkey::PKey;
-use rustls::{pki_types::{CertificateDer, PrivateKeyDer}, RootCertStore, ServerConfig};
+use rcgen::{generate_simple_self_signed, CertifiedKey};
+use rustls::{client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier}, crypto::aws_lc_rs::sign::any_supported_type, pki_types::{CertificateDer, PrivateKeyDer}, server::{danger::{ClientCertVerified, ClientCertVerifier}, ResolvesServerCert}, sign::SigningKey, RootCertStore, ServerConfig};
 use serde_json::{from_str, to_string, to_value, Map, Value};
 use serde::{Deserialize, Serialize};
 use tokio::{io::{self, BufReader}, select};
 use tokio::sync::mpsc::{Receiver, Sender};
-use rsa::{RsaPrivateKey};
+use rsa::{traits::SignatureScheme, RsaPrivateKey};
 use crate::{blockchain::BlockchainRequest, database::get_key_pair};
 
 const DEFAULT_PORT: i32 = 24195;
@@ -53,10 +54,60 @@ pub async fn initialize_p2p_thread(mut receiver_from_blockchain: Receiver<String
 
 }
 
+#[derive(Debug)]
+struct AllowAnyCertVerifier;
+
+impl ServerCertVerifier for AllowAnyCertVerifier {
+    fn verify_server_cert(
+            &self,
+            end_entity: &CertificateDer<'_>,
+            intermediates: &[CertificateDer<'_>],
+            server_name: &rustls::pki_types::ServerName<'_>,
+            ocsp_response: &[u8],
+            now: rustls::pki_types::UnixTime,
+        ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+    
+    fn verify_tls12_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+    
+    fn verify_tls13_signature(
+        &self,
+        message: &[u8],
+        cert: &CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+    
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        vec![]
+    }
+}
+
+impl ResolvesServerCert for AllowAnyCertVerifier {
+    fn resolve(&self, client_hello: rustls::server::ClientHello) -> Option<Arc<rustls::sign::CertifiedKey>> {
+        // Create a dummy server certificate and private key
+        
+        let cert_key = generate_simple_self_signed(vec!["localhost".to_string()]).unwrap();
+        let cert_der = cert_key.cert.der().to_owned();
+    
+        // Return the dummy server certificate and private key as a CertifiedKey
+        Some(Arc::new(rustls::sign::CertifiedKey::new(vec![cert_der], any_supported_type(&PrivateKeyDer::Pkcs8(cert_key.key_pair.serialized_der().into())).unwrap())))
+    }
+}
+
 async fn handle_request_from_network(mut sender_to_blockchain: Sender<String>, key: PrivateKeyDer<'static>, cert: CertificateDer<'static>){
     let config = ServerConfig::builder()
         .with_no_client_auth()
-        .with_single_cert(vec![cert], key).unwrap();
+        .with_cert_resolver(Arc::new(AllowAnyCertVerifier));
 
     let listener = TcpListener::bind("0.0.0.0:8047").unwrap();
 
@@ -75,14 +126,15 @@ async fn handle_request_from_blockchain(mut receiver_from_blockchain: Receiver<S
             println!("Recieved message from blockchain to network");
             let blockchain_request: P2PRequest = from_str(&msg).unwrap();
 
-            let root_store = RootCertStore::empty();
+            let mut root_store = RootCertStore::empty();
+            let _ = root_store.add(cert.clone());
             let mut config = rustls::ClientConfig::builder()
-                .with_root_certificates(root_store)
-                .with_client_auth_cert(vec![cert.clone()], key.clone_key()).unwrap();
+                .dangerous().with_custom_certificate_verifier(Arc::new(AllowAnyCertVerifier))
+                .with_no_client_auth();
 
             let server_name = "localhost".try_into().unwrap();
             let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
-            let mut sock = TcpStream::connect("102.168.2.128:8047").unwrap();
+            let mut sock = TcpStream::connect("192.168.2.128:8047").unwrap();
             let mut tls = rustls::Stream::new(&mut conn, &mut sock);
 
             tls.write_all(b"Hi there!").unwrap();
